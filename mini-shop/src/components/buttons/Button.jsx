899cca918d1cfb1f14172265d81be4c8ec518c22
pouya-abdoli <pouyaabdoli1234@gmail.com>
2026-09-22 00:41:51 +0330

    style: add hover, rounded-md, and transition to Button

--- mini-shop/src/components/buttons/Button.jsx
+++ mini-shop/src/components/buttons/Button.jsx
@@ -1,15 +1,15 @@
 const Button = ({ children, variant = "primary", className = "", ...rest }) => {
   const variants = {
-    primary: "bg-blue-500 text-white",
-    secondary: "bg-gray-500 text-white",
-    danger: "bg-red-500 text-white",
-    warning: "bg-yellow-500 text-black",
-    success: "bg-green-500 text-white",
+    primary: "bg-blue-500 hover:bg-blue-600 text-white",
+    secondary: "bg-gray-500 hover:bg-gray-600 text-white",
+    danger: "bg-red-500 hover:bg-red-600 text-white",
+    warning: "bg-yellow-500 hover:bg-yellow-600 text-black",
+    success: "bg-green-500 hover:bg-green-600 text-white",
   };
 
   return (
     <button
-      className={`px-4 py-2 rounded ${className} ${variants[variant]}`}
+      className={` px-3 py-1 rounded-md transition ${className} ${variants[variant]}`}
       {...rest}>
       {children}
     </button>
